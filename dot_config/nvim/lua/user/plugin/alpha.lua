0cd8a2d852ace10ccd1685c7bbfa67f9fe0a11fa
王斌 <bingo084@outlook.com>
2022-11-30 11:52:30 +0800

    refactor: rewrite alpha-nvim config

--- dot_config/nvim/lua/user/plugin/alpha.lua
+++ dot_config/nvim/lua/user/plugin/alpha.lua
@@ -7,56 +7,98 @@ end
 local icons = require "user.icons"
 
 local dashboard = require "alpha.themes.dashboard"
-local function header()
-    local handle = io.popen("fortune|cowsay")
-    local cowsay = handle:read("*a")
-    handle:close()
-    return cowsay
+
+local function pick_color()
+    local colors = { "String", "Identifier", "Keyword", "Number" }
+    return colors[math.random(#colors)]
 end
 
-local term = require 'alpha.term'
-dashboard.section.terminal.command = "fortune | cowsay"
-dashboard.section.terminal.width = 69
-dashboard.section.terminal.width = 8
--- dashboard.section.header.val = header()
--- dashboard.section.header.val = {
---     [[                               __                ]],
---     [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
---     [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
---     [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
---     [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
---     [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
--- }
-dashboard.section.buttons.val = {
-    dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
-    dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button(
-        "p",
-        icons.git.Repo .. " Find project",
-        ":lua require('telescope').extensions.projects.projects()<CR>"
-    ),
-    dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-    dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("s", icons.ui.SignIn .. " Find Session", ":SearchSession<CR>"),
-    dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-    dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
-    dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+local function info_value()
+    local plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
+    local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+    local version = vim.version()
+    local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+    return datetime .. "   " .. plugins_count .. " plugins" .. nvim_version_info
+end
+
+local logo_value = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
 }
--- local function footer()
---     -- NOTE: requires the fortune-mod package to work
---     local handle = io.popen("fortune")
---     local fortune = handle:read("*a")
---     handle:close()
---     return fortune
---     -- return "chrisatmachine.com"
--- end
 
--- dashboard.section.footer.val = footer()
+local fortune_value = require('alpha.fortune')
 
--- dashboard.section.footer.opts.hl = "Constant"
-dashboard.section.header.opts.hl = "Macro"
-dashboard.section.buttons.opts.hl = "Macro"
+local logo = {
+    type = 'text',
+    val = logo_value,
+    opts = {
+        position = 'center',
+        hl = pick_color(),
+    },
+}
 
-dashboard.config.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
-alpha.setup(dashboard.opts)
+local info = {
+    type = 'text',
+    val = info_value(),
+    opts = {
+        position = 'center',
+        hl = pick_color(),
+    },
+}
+
+local fortune = {
+    type = 'text',
+    val = fortune_value,
+    opts = {
+        position = 'center',
+        hl = pick_color(),
+    },
+}
+
+local buttons = {
+    type = "group",
+    val = {
+        dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button(
+            "p",
+            icons.git.Repo .. " Find project",
+            ":lua require('telescope').extensions.projects.projects()<CR>"
+        ),
+         { type = 'padding', val = 1 },
+        dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button("s", icons.ui.SignIn .. " Find Session", ":SearchSession<CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
+         { type = 'padding', val = 1 },
+        dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+    },
+    position = "center",
+}
+
+local config = {
+    layout = {
+        { type = 'padding', val = 6 },
+        logo,
+        { type = 'padding', val = 2 },
+        info,
+        { type = 'padding', val = 2 },
+        buttons,
+        { type = 'padding', val = 2 },
+        fortune,
+    }
+}
+alpha.setup(config)

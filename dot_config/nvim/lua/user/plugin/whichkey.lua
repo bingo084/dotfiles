f64199c3ee32864e0fb99c319f30dfc052b16d0d
王斌 <bingo084@outlook.com>
2022-11-30 14:17:24 +0800

    refactor: change nvim auto-session search to telescope

--- dot_config/nvim/lua/user/plugin/whichkey.lua
+++ dot_config/nvim/lua/user/plugin/whichkey.lua
@@ -179,7 +179,7 @@ local mappings = {
         s = { "<cmd>SaveSession<cr>", "Save" },
         r = { "<cmd>RestoreSession<cr>", "Restore" },
         x = { "<cmd>DeleteSession<cr>", "Delete" },
-        f = { "<cmd>Autosession search<cr>", "Find" },
+        f = { "<cmd>SearchSession<cr>", "Find" },
         d = { "<cmd>Autosession delete<cr>", "Find Delete" },
     },
 

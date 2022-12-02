85fcaeb2ee6062679ebf626a915f7a2f5e5bf883
王斌 <bingo084@outlook.com>
2022-12-02 14:54:48 +0800

    style: format nvim cmp.lua

--- dot_config/nvim/lua/user/plugin/cmp.lua
+++ dot_config/nvim/lua/user/plugin/cmp.lua
@@ -1,11 +1,11 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
+    return
 end
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
-  return
+    return
 end
 
 local compare = require "cmp.config.compare"
@@ -13,8 +13,8 @@ local compare = require "cmp.config.compare"
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local icons = require "user.icons"
@@ -27,138 +27,139 @@ vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
 vim.api.nvim_set_hl(0, "CmpItemKindCrate", { fg = "#F64D00" })
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-
-  mapping = cmp.mapping.preset.insert {
-    ["<C-k>"] = cmp.mapping(cmp.select_prev_item(), { "i", "c" }),
-    ["<C-j>"] = cmp.mapping(cmp.select_next_item(), { "i", "c" }),
-    ["<C-b>"] = cmp.mapping(cmp.scroll_docs(-1), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.scroll_docs(1), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.complete(), { "i", "c" }),
-    -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-c>"] = cmp.mapping {
-      i = cmp.abort(),
-      c = cmp.close(),
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
     },
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.confirm { select = true },
-    ["<Right>"] = cmp.confirm { select = true },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif has_words_before() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      -- Kind icons
-      vim_item.kind = kind_icons[vim_item.kind]
 
-      if entry.source.name == "cmp_tabnine" then
-        vim_item.kind = icons.misc.Robot
-        vim_item.kind_hl_group = "CmpItemKindTabnine"
-      end
-      if entry.source.name == "copilot" then
-        vim_item.kind = icons.git.Octoface
-        vim_item.kind_hl_group = "CmpItemKindCopilot"
-      end
-
-      if entry.source.name == "emoji" then
-        vim_item.kind = icons.misc.Smiley
-        vim_item.kind_hl_group = "CmpItemKindEmoji"
-      end
-
-      if entry.source.name == "crates" then
-        vim_item.kind = icons.misc.Package
-        vim_item.kind_hl_group = "CmpItemKindCrate"
-      end
-
-      -- NOTE: order matters
-      vim_item.menu = ({
-        nvim_lsp = "Lsp",
-        nvim_lua = "Lua",
-        luasnip = "Luasnip",
-        buffer = "Buffer",
-        path = "Path",
-        emoji = "Emoji",
-      })[entry.source.name]
-      return vim_item
-    end,
-  },
-  sources = {
-    { name = "crates", group_index = 1 },
-    { name = "nvim_lsp", group_index = 2 },
-    { name = "nvim_lua", group_index = 2 },
-    { name = "luasnip", group_index = 2 },
-    { name = "buffer", group_index = 2 },
-    { name = "path", group_index = 2 },
-    { name = "emoji", group_index = 2 },
-  },
-  sorting = {
-    priority_weight = 2,
-    comparators = {
-      -- require("copilot_cmp.comparators").prioritize,
-      -- require("copilot_cmp.comparators").score,
-      compare.offset,
-      compare.exact,
-      -- compare.scopes,
-      compare.score,
-      compare.recently_used,
-      compare.locality,
-      -- compare.kind,
-      compare.sort_text,
-      compare.length,
-      compare.order,
-      -- require("copilot_cmp.comparators").prioritize,
-      -- require("copilot_cmp.comparators").score,
+    mapping = cmp.mapping.preset.insert {
+        ["<C-k>"] = cmp.mapping(cmp.select_prev_item(), { "i", "c" }),
+        ["<C-j>"] = cmp.mapping(cmp.select_next_item(), { "i", "c" }),
+        ["<C-b>"] = cmp.mapping(cmp.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.scroll_docs(1), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.complete(), { "i", "c" }),
+        -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ["<C-c>"] = cmp.mapping {
+            i = cmp.abort(),
+            c = cmp.close(),
+        },
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.confirm { select = true },
+        ["<Right>"] = cmp.confirm { select = true },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.jumpable(1) then
+                luasnip.jump(1)
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif has_words_before() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
     },
-  },
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
-  window = {
-    documentation = {
-      border = "rounded",
-      winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = kind_icons[vim_item.kind]
+
+            if entry.source.name == "cmp_tabnine" then
+                vim_item.kind = icons.misc.Robot
+                vim_item.kind_hl_group = "CmpItemKindTabnine"
+            end
+
+            if entry.source.name == "copilot" then
+                vim_item.kind = icons.git.Octoface
+                vim_item.kind_hl_group = "CmpItemKindCopilot"
+            end
+
+            if entry.source.name == "emoji" then
+                vim_item.kind = icons.misc.Smiley
+                vim_item.kind_hl_group = "CmpItemKindEmoji"
+            end
+
+            if entry.source.name == "crates" then
+                vim_item.kind = icons.misc.Package
+                vim_item.kind_hl_group = "CmpItemKindCrate"
+            end
+
+            -- NOTE: order matters
+            vim_item.menu = ({
+                nvim_lsp = "Lsp",
+                nvim_lua = "Lua",
+                luasnip = "Luasnip",
+                buffer = "Buffer",
+                path = "Path",
+                emoji = "Emoji",
+            })[entry.source.name]
+            return vim_item
+        end,
     },
-    completion = {
-      border = "rounded",
-      winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+    sources = {
+        { name = "crates", group_index = 1 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "path", group_index = 2 },
+        { name = "emoji", group_index = 2 },
     },
-  },
-  experimental = {
-    ghost_text = true,
-  },
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            -- require("copilot_cmp.comparators").prioritize,
+            -- require("copilot_cmp.comparators").score,
+            compare.offset,
+            compare.exact,
+            -- compare.scopes,
+            compare.score,
+            compare.recently_used,
+            compare.locality,
+            -- compare.kind,
+            compare.sort_text,
+            compare.length,
+            compare.order,
+            -- require("copilot_cmp.comparators").prioritize,
+            -- require("copilot_cmp.comparators").score,
+        },
+    },
+    confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    },
+    window = {
+        documentation = {
+            border = "rounded",
+            winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+        },
+        completion = {
+            border = "rounded",
+            winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+        },
+    },
+    experimental = {
+        ghost_text = true,
+    },
 }

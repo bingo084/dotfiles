5400667f145b120e47be114e1c79f27cb024c503
王斌 <bingo084@outlook.com>
2024-04-04 23:58:40 +0800

    feat(chezmoi): extract private repo to chezmoi data

--- run_once_after_clone_private_repo.sh
+++ run_once_after_clone_private_repo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-git clone git@github.com:bingo084/nvim-config.git .config/nvim
-git clone git@github.com:bingo084/script.git bin
-git clone git@github.com:bingo084/rime.git .config/rime
+{{ range .privateRepo -}}
+git clone git@github.com:bingo084/{{ .name }}.git {{ .target }}
+{{ end -}}

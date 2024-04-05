c6592ce19fc47bc26fcb4a1ea1549f7cea2b959d
王斌 <bingo084@outlook.com>
2024-04-05 11:53:34 +0800

    fix(chezmoi): only clone private repo when target doesn't exist

--- run_once_after_clone_private_repo.sh
+++ run_once_after_clone_private_repo.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-{{ range .privateRepo -}}
+{{- range .privateRepo }}
+{{- if not (stat (joinPath $.chezmoi.homeDir .target)) }}
 git clone git@github.com:bingo084/{{ .name }}.git {{ .target }}
-{{ end -}}
+{{- end }}
+{{- end }}

eec8c20a88b67c2d02c7270087d734ace92723dd
王斌 <bingo084@outlook.com>
2024-04-10 10:49:53 +0800

    feat(fzf): add preview scroll page keybinds

--- dot_config/zsh/executable_fzf-config.sh
+++ dot_config/zsh/executable_fzf-config.sh
@@ -5,6 +5,7 @@ FZF_THEME=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 export FZF_DEFAULT_OPTS="$FZF_THEME --height ~50% --reverse --border --inline-info --ansi \
+    --bind=alt-d:preview-half-page-down,alt-u:preview-half-page-up \
     --select-1 --exit-0"
 
 export FZF_COMPLETION_TRIGGER='\'

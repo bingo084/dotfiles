77e35c144e44d09937132cb45c905c456cfe3359
王斌 <bingo084@outlook.com>
2024-10-08 17:40:54 +0800

    add brightness, coffee, hardware

--- dot_config/ags/config.js
+++ dot_config/ags/config.js
@@ -9,25 +9,24 @@ const dispatch = (
 const activeId = hyprland.active.workspace.bind("id");
 const defaultIds = [1, 2, 3, 4, 5];
 
-const Workspaces = () =>
-  Widget.EventBox({
-    className: "workspaces",
-    onScrollUp: () => dispatch("workspace", "-1"),
-    onScrollDown: () => dispatch("workspace", "+1"),
-    child: Widget.Box({
-      children: hyprland.bind("workspaces").as((ws) =>
-        [...new Set([...defaultIds, ...ws.map(({ id }) => id)])]
-          .sort((a, b) => a - b)
-          .map((id) =>
-            Widget.Button({
-              className: activeId.as((i) => (i === id ? "focused" : "")),
-              onClicked: () => dispatch("workspace", id),
-              child: Widget.Label(`${id}`),
-            }),
-          ),
-      ),
-    }),
-  });
+const Workspaces = Widget.EventBox({
+  className: "workspaces",
+  onScrollUp: () => dispatch("workspace", "-1"),
+  onScrollDown: () => dispatch("workspace", "+1"),
+  child: Widget.Box({
+    children: hyprland.bind("workspaces").as((ws) =>
+      [...new Set([...defaultIds, ...ws.map(({ id }) => id)])]
+        .sort((a, b) => a - b)
+        .map((id) =>
+          Widget.Button({
+            className: activeId.as((i) => (i === id ? "focused" : "")),
+            onClicked: () => dispatch("workspace", id),
+            child: Widget.Label(`${id}`),
+          }),
+        ),
+    ),
+  }),
+});
 
 const { query } = await Service.import("applications");
 const activeClient = hyprland.active.client.bind("address");
@@ -47,7 +46,6 @@ const Clients = Widget.Box({
           className: activeClient.as((a) => (a === address ? "focused" : "")),
           onClicked: () => dispatch("focuswindow", `address:${address}`),
           onMiddleClick: () => dispatch("closewindow", `address:${address}`),
-          // onHover: () => console.log(at),
           child: Widget.Icon(query(clazz)[0]?.icon_name || "image-missing"),
           tooltipText: title,
         }),
@@ -61,6 +59,55 @@ const ClientTitle = Widget.Label({
   visible: hyprland.active.client.bind("address").as((addr) => !!addr),
 });
 
+const divide = ([total, free]) => free / total;
+
+const cpu = Variable(0, {
+  poll: [
+    2000,
+    "top -b -n 1",
+    (out) =>
+      divide([
+        100,
+        out
+          .split("\n")
+          .find((line) => line.includes("Cpu(s)"))
+          .split(/\s+/)[1]
+          .replace(",", "."),
+      ]),
+  ],
+});
+
+const ram = Variable(0, {
+  poll: [
+    2000,
+    "free",
+    (out) =>
+      divide(
+        out
+          .split("\n")
+          .find((line) => line.includes("Mem:"))
+          .split(/\s+/)
+          .splice(1, 2),
+      ),
+  ],
+});
+
+const Hardware = Widget.ListBox({
+  className: "hardware",
+  setup(self) {
+    self.add(
+      Widget.Label({
+        label: cpu.bind().as((v) => ` ${(v * 100).toFixed(0)}%`),
+      }),
+    );
+    self.add(
+      Widget.Label({
+        label: ram.bind().as((v) => ` ${(v * 100).toFixed(0)}%`),
+      }),
+    );
+  },
+});
+
 const date = Variable("", {
   poll: [1000, "date '+%a, %b %d  %H:%M'"],
 });
@@ -68,6 +115,22 @@ const date = Variable("", {
 const Clock = Widget.Label({
   className: "clock",
   label: date.bind(),
+});
+
+const hypridle = Variable(Utils.exec("pidof hypridle"));
+
+const Coffee = Widget.EventBox({
+  className: "coffee",
+  onPrimaryClick: () => {
+    hypridle.value
+      ? Utils.exec("killall hypridle")
+      : Utils.subprocess("hypridle");
+    hypridle.value = Utils.exec("pidof hypridle");
+  },
+  child: Widget.Icon({
+    css: hypridle.bind().as((p) => (p ? " " : "color: red")),
+    icon: "preferences-desktop-screensaver",
+  }),
 });
 
 const audio = await Service.import("audio");
@@ -78,7 +141,6 @@ const Volume = Widget.EventBox({
   onPrimaryClick: () => (audio.speaker.is_muted = !audio.speaker.is_muted),
   onScrollUp: () => changeVolume(1),
   onScrollDown: () => changeVolume(-1),
-  on_hover: () => console.log(audio.speaker),
   child: Widget.Box({
     tooltipText: audio.speaker.bind("description").as((d) => `${d}`),
     children: [
@@ -101,6 +163,39 @@ const Volume = Widget.EventBox({
           .as((volume) => ` ${Math.round(volume * 100)}%`),
       }),
     ],
+  }),
+});
+
+import brightness from "./brightness.js";
+
+const brightnessIcons = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+];
+const icon = (/** @type {number} */ v, /** @type {string[]} */ icons) =>
+  icons[Math.round((v / 100) * (icons.length - 1))];
+
+const Brightness = Widget.EventBox({
+  className: "brightness",
+  onScrollUp: () => (brightness.screen_value += 0.01),
+  onScrollDown: () => (brightness.screen_value -= 0.01),
+  child: Widget.Label({
+    label: brightness
+      .bind("screen_value")
+      .as((v) => `${icon(v * 100, brightnessIcons)}  ${Math.round(v * 100)}%`),
   }),
 });
 
@@ -130,15 +225,7 @@ const deviceMap = {
   "input-mouse": "  ",
   "audio-headset": " ",
 };
-const batteryIcons = [
-  [80, ""],
-  [60, ""],
-  [40, ""],
-  [20, ""],
-  [0, ""],
-];
-const batteryIcon = (/** @type {number} */ percent) =>
-  `${batteryIcons.find(([threshold]) => threshold <= percent)?.[1]} ${percent}%`;
+const batteryIcons = ["", "", "", "", ""];
 const Bluetooth = Widget.EventBox({
   onPrimaryClick: () => (bluetooth.enabled = !bluetooth.enabled),
   tooltipText: bluetooth
@@ -147,7 +234,7 @@ const Bluetooth = Widget.EventBox({
       devices
         .map(
           ({ icon_name, name, battery_percentage }) =>
-            `${deviceMap[icon_name] || " "} ${name}  ${batteryIcon(battery_percentage)}`,
+            `${deviceMap[icon_name] || " "} ${name}  ${icon(battery_percentage, batteryIcons)} ${battery_percentage}%`,
         )
         .join("\n"),
     ),
@@ -177,54 +264,56 @@ const Network = Widget.Stack({
   shown: network.bind("primary").as((p) => p || "wifi"),
 });
 
-function SysTray() {
-  const items = systemtray.bind("items").as((items) =>
+const SysTray = Widget.Box({
+  children: systemtray.bind("items").as((items) =>
     items.map((item) =>
       Widget.Button({
         child: Widget.Icon({ icon: item.bind("icon") }),
-        on_primary_click: (_, event) => item.activate(event),
-        on_secondary_click: (_, event) => item.openMenu(event),
-        tooltip_markup: item.bind("tooltip_markup"),
+        onPrimaryClick: (_, event) => item.activate(event),
+        onSecondaryClick: (_, event) => item.openMenu(event),
+        tooltipMarkup: item.bind("tooltip_markup"),
       }),
     ),
-  );
+  ),
+});
 
-  return Widget.Box({
-    children: items,
-  });
-}
+const Left = Widget.Box({
+  spacing: 8,
+  children: [Workspaces, Clients, ClientTitle],
+});
 
-// layout of the bar
-const Left = () =>
-  Widget.Box({
-    spacing: 8,
-    children: [Workspaces(), Clients, ClientTitle],
-  });
+const Center = Widget.Box({
+  spacing: 8,
+  children: [],
+});
 
-const Center = () =>
-  Widget.Box({
-    spacing: 8,
-    children: [],
-  });
-
-const Right = () =>
-  Widget.Box({
-    hpack: "end",
-    spacing: 8,
-    children: [Network, Bluetooth, Volume, Battery, Clock, SysTray()],
-  });
+const Right = Widget.Box({
+  hpack: "end",
+  spacing: 8,
+  children: [
+    Hardware,
+    Network,
+    Bluetooth,
+    Coffee,
+    Volume,
+    Brightness,
+    Battery,
+    Clock,
+    SysTray,
+  ],
+});
 
 const Bar = (monitor = 0) =>
   Widget.Window({
-    name: `bar-${monitor}`, // name has to be unique
-    class_name: "bar",
+    name: `bar-${monitor}`,
+    className: "bar",
     monitor,
     anchor: ["top", "left", "right"],
     exclusivity: "exclusive",
     child: Widget.CenterBox({
-      start_widget: Left(),
-      center_widget: Center(),
-      end_widget: Right(),
+      startWidget: Left,
+      centerWidget: Center,
+      endWidget: Right,
     }),
   });
 

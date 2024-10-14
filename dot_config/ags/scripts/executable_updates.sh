bbe675ee5253487a2082255270469e0158bacff4
王斌 <bingo084@outlook.com>
2024-10-14 16:40:44 +0800

    fix(ags): wrong count when no updates

--- dot_config/ags/scripts/executable_updates.sh
+++ dot_config/ags/scripts/executable_updates.sh
@@ -4,7 +4,7 @@ interval=${1:-3600}
 
 while true; do
   packages=$(checkupdates 2>/dev/null)
-  count=$(($(echo -n "$packages" | wc -l) + $(paru -Qua 2>/dev/null | wc -l)))
+  count=$(($(echo "$packages" | grep -vc ^\$) + $(paru -Qua 2>/dev/null | wc -l)))
   packages_array=$(echo -n "$packages" | awk '{printf("{\"name\": \"%s\", \"old_version\": \"%s\", \"new_version\": \"%s\"}", $1, $2, $NF)}' | jq -s .)
   jq --unbuffered --null-input --compact-output \
     --argjson count "$count" \

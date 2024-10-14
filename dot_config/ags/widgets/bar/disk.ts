52761397c1cca147e2885f7100d81df14144b694
王斌 <bingo084@outlook.com>
2024-10-14 16:41:17 +0800

    feat(ags): add tooltip text for disk

--- dot_config/ags/widgets/bar/disk.ts
+++ dot_config/ags/widgets/bar/disk.ts
@@ -2,6 +2,12 @@ import { disk } from "variables";
 
 export default Widget.Box({
   spacing: 8,
+  tooltipText: disk
+    .bind()
+    .as(
+      ({ size, used, use, mount_on }) =>
+        `${used} used out of ${size} on ${mount_on} (${use})`,
+    ),
   children: [
     Widget.Icon("drive-harddisk-symbolic"),
     Widget.Label({

961260acd51e3dad77599bb2927f3c5fa0f02afa
bingo <bingo084@outlook.com>
2026-08-30 00:53:18 +0800

    refactor(hyprland): move monitors.lua into modules

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -1,5 +1,3 @@
-require("monitors")
-
 require("modules.animations")
 require("modules.appearance")
 require("modules.autostart")
@@ -8,6 +6,7 @@ require("modules.env")
 require("modules.gestures")
 require("modules.input")
 require("modules.layouts")
+require("modules.monitors")
 require("modules.misc")
 require("modules.plugins")
 require("modules.rules")

78c7cf95af5b4826ab2745efd459abd334289eae
bingo <bingo084@outlook.com>
2026-08-30 00:49:19 +0800

    feat(hyprland): auto update hyprpm when reload failed

--- dot_config/hypr/modules/autostart.lua
+++ dot_config/hypr/modules/autostart.lua
@@ -5,5 +5,14 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("Telegram", { workspace = 2 })
 	-- Start rbw-agent to SSH requests
 	hl.exec_cmd("rbw sync")
-	hl.exec_cmd("hyprpm reload")
+	hl.exec_cmd([[
+    hyprpm reload || kitty --class hyprpm-update sh -lc '
+      if hyprpm update; then
+        hyprctl reload
+      else
+        printf "\nUpdate failed. Press Enter to close."
+        read -r _
+      fi
+    '
+  ]])
 end)

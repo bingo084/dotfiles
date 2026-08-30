8401ada375f3e3f42178ab31e1b1b4f318e4b0c0
bingo <bingo084@outlook.com>
2026-08-30 22:59:43 +0800

    feat(hyprland): refine borderless windows and group indicators

--- dot_config/hypr/modules/appearance.lua
+++ dot_config/hypr/modules/appearance.lua
@@ -3,18 +3,33 @@ local colors = require("generated.colors")
 hl.config({
 	-- https://wiki.hypr.land/Configuring/Basics/Variables/#general
 	general = {
-		border_size = 2,
-		gaps_in = 2,
-		gaps_out = 4,
-		col = { inactive_border = colors.outline_variant[100], active_border = colors.primary[80] },
+		border_size = 0,
+		gaps_in = 3,
+		gaps_out = 6,
 	},
 	-- https://wiki.hypr.land/Configuring/Basics/Variables/#decoration
 	decoration = {
 		rounding = 10,
 		rounding_power = 4.0,
 		blur = { size = 5, passes = 3, input_methods = true },
-		shadow = { range = 60, color = "0xaa1a1a1a", color_inactive = "0x331a1a1a" },
+		shadow = {
+			range = 60,
+			color = colors.shadow[40],
+			color_inactive = colors.shadow[20],
+		},
 	},
 	-- https://wiki.hypr.land/Configuring/Basics/Variables/#group
-	group = { groupbar = { font_size = 12 } },
+	group = {
+		groupbar = {
+			render_titles = false,
+			rounding = 3,
+			round_only_edges = false,
+			col = {
+				active = colors.primary[100],
+				inactive = colors.primary[30],
+				locked_active = colors.error[100],
+				locked_inactive = colors.error[30],
+			},
+		},
+	},
 })
